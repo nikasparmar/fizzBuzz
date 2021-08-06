274b20e036cb93e4fbf0ccdfef89a3289e4c6682
nikas <nikas@antarcticaglobal.com>
2021-08-06 12:13:21 +0530

    change port

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ if (cluster.isMaster) {
   });
 }
 else {
-  app.listen(3000, () => console.log('server starting on port 3000!'));
+  app.listen(process.env.PORT || 3000, () => console.log('server starting'));
 }
